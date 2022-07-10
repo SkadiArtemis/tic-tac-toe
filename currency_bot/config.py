afec2596974b24318e9b269629831deadc6df0e2
Anna Glazunova <79213278172@yandex.ru>
2022-07-10 18:16:17 +0400

    add file

--- currency_bot/config.py
+++ currency_bot/config.py
@@ -1,6 +1,6 @@
 TOKEN = "5404492428:AAEmkmsj47X1OUPBMzjD6FfjqmN61l_jl_w"
 exchanges = {
-    'Доллар': USD
-    'Евро': EUR
-    'Рубль': RUB
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB'
 }
