a0648becdc1e80d833cab59ef5053d7ae23a4b71
Anna Glazunova <79213278172@yandex.ru>
2022-08-02 12:38:33 +0400

    add

--- test_1.py
+++ test_1.py
@@ -1,8 +1,49 @@
-import requests
-import json
+@bot.message_handler(content_types=['text', ])
+def converter(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
 
-r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')
+        if len(values) !=3:
+            raise APIException("Неверное количество параметров!")
 
-r = json.loads(r.content)
+        base, sym, amount = values
+        total_base = Convertor.convert(base, sym, amount)
 
-print(r[0])
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя!\n{e}")
+
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду: \n{e}")
+    else:
+        text = f"Ценa {amount} {base} в {sym} - {total_base}"
+
+
+
+
+
+
+
+bot.polling()
+
+
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise APIException("Неверное количество параметров!")
+
+        base, sym, amount = values
+        total_base = CurrencyConvertor.get_price(base, sym, amount)
+
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя!\n{e}")
+
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду: \n{e}")
+    else:
+        text = f"Ценa {amount} {base} в {sym} - {total_base}"
+
--- test_3.py
+++ test_3.py
@@ -1,1 +1,8 @@
-print("Hello!")
+import requests
+import json
+base = 'USD'
+res = 'EUR'
+r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={base}&tsyms={res}")
+total_base = json.loads(r.content) # преобразовываем полученные данные в словарь
+print(total_base) # смотрим как выглядит словарь и какие ключи
+print(total_base[res]) # применяем ключ
