407b6c2ab7acdbc16203b27d6021eaeb111d382f
Anna Glazunova <79213278172@yandex.ru>
2022-08-20 12:51:30 +0400

    add

--- news_dir/project/project/news_app/models.py
+++ news_dir/project/project/news_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-class Order(models.Model):
-    pass
-
-#CREATE TABLE PRODUCTS (
-#    product_id INT AUTO_INCREMENT NOT NULL,
-#    name CHAR(255) NOT NULL,
-#    price FLOAT NOT NULL,
-#
-#    PRIMARY KEY (product_id)
-#);
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
@@ -29,13 +19,28 @@ POSITIONS = [
     (cashier, 'Кассир'),
     (cleaner, 'Уборщик')
 ]
+
+
 class Staff(models.Model):
     full_name = models.CharField(max_length=255)
     position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
     labor_contract = models.IntegerField()
 
 
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+    products = models.ManyToManyField(Product, through='ProductOrder')
+
 
 class ProductOrder(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+
 
