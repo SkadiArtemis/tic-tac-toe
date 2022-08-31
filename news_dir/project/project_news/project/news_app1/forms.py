8e09996b88b6a7354db5d1d813ad213410da53c1
Anna Glazunova <79213278172@yandex.ru>
2022-09-01 00:46:53 +0400

    add

---
+++ news_dir/project/project_news/project/news_app1/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core. exceptions import ValidationError
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+        def clean(self):
+            cleaned_data = super().clean()
+            name = cleaned_data.get("name")
+            description = cleaned_data.get("description")
+
+            if name == description:
+                raise ValidationError(
+                    "Описание не должно быть идентично названию."
+                )
+
+            return cleaned_data
+
--- news_dir/project/project_news/project/news_app1/views.py
+++ news_dir/project/project_news/project/news_app1/views.py
@@ -4,6 +4,9 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView, CreateView
 from django.shortcuts import get_object_or_404
 from .models import *
+from .filters import PostFilter
+from .forms import PostForm
+from .models import Post
 
 class AuthorList(ListView):
     model = Author
@@ -12,8 +15,19 @@ class AuthorList(ListView):
 
 class Post(DetailView):
     model = Post
+    template_name = 'news.html'
     context_object_name = 'Post'
 
 class PostCategory(CreateView):
     model = Post
     fields = ['__all__']
+
+class ProductCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.quantity = 13
+        return super().form_valid(form)
