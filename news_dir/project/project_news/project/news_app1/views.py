1ea06076a7326404e20d78f7862a024780d656e0
Anna Glazunova <79213278172@yandex.ru>
2022-09-09 18:23:13 +0400

    add

--- news_dir/project/project_news/project/news_app1/views.py
+++ news_dir/project/project_news/project/news_app1/views.py
@@ -53,8 +53,8 @@ class BaseRegisterView(CreateView):
 @ login_required
 def upgrade_me(request):
     user = request.user
-    premium_group = Group.objects.get(name='premium')
-    if not request.user.groups.fillter(name='premium').exists():
-        premium_group.user_set.add(user)
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.fillter(name='authors').exists():
+        authors_group.user_set.add(user)
     return redirect('/')
 
